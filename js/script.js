0a30e6ec21d42fe8548cfbf7dab522c10f131339
Muhammad Ali Shahzaib <72030025+imM-Ali@users.noreply.github.com>
2021-05-02 17:00:13 +0100

    part 1 done
    1) fixed regex working without Capitals
    2) Changed password check implementation

--- js/script.js
+++ js/script.js
@@ -1,27 +1,41 @@
 var errorEle = document.getElementById('error');
 var password = document.getElementById('pass');
 var form = document.getElementById('myForm');
+var errMsg;
 
-form.addEventListener('submit', function (e) {
-    var failed=false, message;
-
+//checks the password and sets the error message value accordingly, if no error, then message is empty  
+function check() {
+    errMsg = "";
     if (password.value == null || password.value == "") {
-        message = "Password cannot be blank!";
-        failed = true;
+        errMsg = "Password cannot be blank!";
+
     }
     else if (password.value.length < 8) {
-        message = "Cannot be shorter than 8 characters";
-        failed = true;
+        errMsg = "Cannot be shorter than 8 characters";
+
     }
-    else if (!password.value.match(/^(?=.*\d)(?=.*[a-zA-Z]+)(?=.*[\!\"\£\$\%\^\&\*\(\)]+).{8,}$/)) {
-        message = "Atleast 1 Lowercase, 1 Uppercase, 1 digit and 1 Special character";
-        failed = true;
+    else if (!password.value.match(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/)) {
+        errMsg = "Atleast 1 Lowercase, 1 Uppercase, 1 digit and 1 Special character";
+
     }
-   
-    if (failed) { 
-        e.preventDefault();
-        errorEle.innerHTML = message;
-        errorEle.style.visibility = 'visible';
+}
+//calls to check password after every keystroke and displays message and styling
+password.addEventListener('keyup', function () {
+    check();
+    errorEle.innerHTML = errMsg;
+    errorEle.style.visibility = 'visible';
+    if (errMsg != "") {
         document.querySelector('input').style.borderBottom = '1px solid red';
+        document.getElementById('mybtn').style.display = 'none';
+    } else {
+        document.querySelector('input').style.borderBottom = '1px solid lightgreen';
+        document.getElementById('mybtn').style.display = 'inline-block';
+    }
+
+});
+//prevents form submission if there is an error message
+form.addEventListener('submit', (e) => {
+    if (errMsg != "") {
+        e.preventDefault();
     }
 })
