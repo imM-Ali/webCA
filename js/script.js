fa2281f07bb718ba28b2478b013401ccdf765247
Muhammad Ali Shahzaib <72030025+imM-Ali@users.noreply.github.com>
2021-05-03 15:04:34 +0100

    Passowrd to Customers transition on same page

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 var errorEle = document.getElementById('error');
 var password = document.getElementById('pass');
-var form = document.getElementById('myForm');
+var nxtBtn = document.getElementById('mybtn');
 var errMsg;
 
 //checks the password and sets the error message value accordingly, if no error, then message is empty  
@@ -28,19 +28,38 @@ password.addEventListener('keyup', function () {
     errorEle.style.visibility = 'visible';
     if (errMsg != "") {
         document.querySelector('input').style.borderBottom = '1px solid red';
-        document.getElementById('mybtn').style.display = 'none';
+       nxtBtn.style.display = 'none';
     } else {
         document.querySelector('input').style.borderBottom = '1px solid lightgreen';
-        document.getElementById('mybtn').style.display = 'inline-block';
+        nxtBtn.style.display = 'inline-block';
+        nxtBtn.focus();
 
     }
 
 });
 //prevents form submission if there is an error message
 
-form.addEventListener('submit', (e) => {
+nxtBtn.addEventListener('click', (e) => {
     if (errMsg != "") {
         e.preventDefault();
+    }else{
+        fetch('https://randomuser.me/api/?results=5&inc=name,picture,email')
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById('content').innerHTML ="";
+                for (let i = 0; i < 5; i++) {
+                    var customer = " <div id='user_" + (i + 1) + "' class='col-lg-6 col-md-12' style='text-align: -webkit-center;'>"
+                        + "<div class='card mb-5 shadow rounded' style='width: 20rem;''>"
+                        + "<img class='card-img-top' style='width:60%; align-self: center; margin-top:5px' src='" + data.results[i].picture.large + "'>"
+                        + "<div class='card-body'><h5 class='card-title'>" + data.results[i].name.first + " " + data.results[i].name.last + "</h5></div>"
+                        + "<ul class='list-group list-group-flush'><li class='list-group-item'>" + data.results[i].email + "</li></ul>"
+                        + "<div class='card-body'><a href='#/' id='modalCall_" + i + "' class='card-link'>Take order</a></div></div></div>"
+                    document.getElementById('content').innerHTML += customer;
+
+                }
+
+
+            })
     }
 
 })
