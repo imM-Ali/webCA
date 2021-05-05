a2cb6b214dbd3f4eba54f18c0f056b4eb170b208
Muhammad Ali Shahzaib <72030025+imM-Ali@users.noreply.github.com>
2021-05-05 18:46:31 +0100

    Cart Updated

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ var nxtBtn = document.getElementById('mybtn');
 var errMsg;
 
 /**
- * checks the password and sets the error message value accordingly, if no error, then message is empty  
+ * checks the password and sets the error message value accordingly, if no error, then message is empty
  */
 function passwordCheck() {
     errMsg = "";
@@ -23,6 +23,7 @@ function passwordCheck() {
 
     }
 }
+
 /**
  * calls to check password after every keystroke and displays message and styling
  */
@@ -32,12 +33,11 @@ password.addEventListener('keyup', function() {
     errorEle.style.visibility = 'visible';
     if (errMsg != "") {
         document.querySelector('input').style.borderBottom = '1px solid red';
-        nxtBtn.style.display = 'none';
+        nxtBtn.style.visibility = 'hidden';
     } else {
         document.querySelector('input').style.borderBottom = '1px solid lightgreen';
-        nxtBtn.style.display = 'inline-block';
+        nxtBtn.style.visibility = 'visible';
         nxtBtn.focus();
-
     }
 
 });
@@ -49,26 +49,22 @@ nxtBtn.addEventListener('click', (e) => {
     if (errMsg != "") {
         e.preventDefault();
     } else {
-        document.querySelector('.nav-link').innerHTML = "Customers";
-        fetch('https://randomuser.me/api/?results=5&inc=name,picture,email')
-            .then(response => response.json())
-            .then(data => {
-                document.getElementById('section1').innerHTML = "";
-                //creates a bootstrap cart for each customer to load into
-                for (let i = 0; i < 5; i++) {
-                    var customer = " <div id='user_" + (i + 1) + "' class='col-lg-6 col-md-12' style='text-align: -webkit-center;'>" +
-                        "<div class='card mb-5 shadow rounded' style='width: 20rem;''>" +
-                        "<img class='card-img-top' style='width:60%; align-self: center; margin-top:5px' src='" + data.results[i].picture.large + "'>" +
-                        "<div class='card-body'><h5 class='card-title'>" + data.results[i].name.first + " " + data.results[i].name.last + "</h5></div>" +
-                        "<ul class='list-group list-group-flush'><li class='list-group-item'>" + data.results[i].email + "</li></ul>" +
-                        "</div></div>"
-                    document.getElementById('section1').innerHTML += customer;
-                    document.getElementById('section2').style.visibility = 'visible';
-                }
-
-            })
+        // document.querySelector('.nav-link').innerHTML = "Customers";
+        fetch('https://randomuser.me/api/?results=5&inc=name,picture,email').then(response => response.json()).then(data => {
+            document.getElementById('section1').style.display = 'none';
+            //creates a bootstrap cart for each customer to load into
+            for (let i = 0; i < 5; i++) {
+                var customer = "<div id='user_" + (i + 1) + "' class='col-lg-3 col-md-4 user-card'>" +
+                    "<div class='card mb-5 shadow rounded'>" +
+                    "<img class='card-img-top' src='" + data.results[i].picture.large + "'>" +
+                    "<div class='card-body p-2'><h5 class='card-title'>" + data.results[i].name.first + " " + data.results[i].name.last + "</h5>" +
+                    "<p>" + data.results[i].email + "</p></div>"
+                document.getElementById('section2').innerHTML += customer;
+                document.getElementById('section2').style.display = 'flex';
+                document.getElementById('section3').style.display = 'block';
+            }
+        })
     }
-
 })
 
 /**
@@ -77,9 +73,7 @@ nxtBtn.addEventListener('click', (e) => {
 
 //*food items are stored on an external server in a json file*
 
-
 //getting food objects and populating menu 
-
 fetch('https://api.jsonbin.io/b/609286acd64cd16802ab0af6/4').then(response => response.json()).then(data => {
     var startersfoodContainer = document.getElementsByClassName('starters')[0];
     var mainsfoodContainer = document.getElementsByClassName('mains')[0];
@@ -87,8 +81,9 @@ fetch('https://api.jsonbin.io/b/609286acd64cd16802ab0af6/4').then(response => re
     var drinksfoodContainer = document.getElementsByClassName('drinks')[0];
 
     //creating a new div for each food item and appending to html
-    data.starters.forEach(element => {
-        startersfoodContainer.innerHTML += `
+    function populate(element, foodContainer) {
+        //populating menu
+        foodContainer.innerHTML += `
         <div data-name="${element.name}" data-price="${element.price}" data-type="starters" class="menu-item form-group">
         <strong>${element.name}</strong><br /> - 
         <span style="font-size:0.6em">
@@ -98,47 +93,40 @@ fetch('https://api.jsonbin.io/b/609286acd64cd16802ab0af6/4').then(response => re
          </span>  
          <span class="add-to-cart-btn btn btn-secondary">+
          </span>
-         </div>`
+         </div>`;
+        //populating cart - hidden on initital state
+        var cart = document.getElementById('cart');
+        cart.innerHTML += `<div class="cart-item mt-2 mb-4 d-none">
+        <span class="cart-name">${element.name}</span>    
+        <input class="cart-quantity" placeholder="0" type="number" min="0" max="5" readOnly>
+        <span class="cart-price">${element.price} /pc</span>
+        </div>`
+    }
+
+    data.starters.forEach(element => {
+        populate(element, startersfoodContainer);
     })
     data.mains.forEach(element => {
-        mainsfoodContainer.innerHTML += `<div data-name="${element.name}" data-price="${element.price}" data-type="mains" class="menu-item form-group">
-        <strong>${element.name}</strong><br /> - 
-        <span style="font-size:0.6em">
-        <strong>${element.description}</strong>
-        <span> 
-        <span class="remove-btn btn btn-secondary">-
-        </span> 
-        <span class="add-to-cart-btn btn btn-secondary">+
-        </span>
-        </div>`
+        populate(element, mainsfoodContainer);
     })
     data.desserts.forEach(element => {
-        dessertsfoodContainer.innerHTML += `<div data-name="${element.name}" data-price="${element.price}" data-type="desserts" class="menu-item form-group">
-        <strong>${element.name}</strong><br /> - 
-        <span style="font-size:0.6em">
-        <strong>${element.description}</strong>
-        <span>   
-        <span class="remove-btn btn btn-secondary">-
-        </span>  
-        <span class="add-to-cart-btn btn btn-secondary">+
-        </span>
-        </div>`
+        populate(element, dessertsfoodContainer);
     })
     data.drinks.forEach(element => {
-        drinksfoodContainer.innerHTML += `<div data-name="${element.name}" data-price="${element.price}" data-type="drinks" class="menu-item form-group"><strong>${element.name}</strong><br /> - <span style="font-size:0.6em"><strong>${element.description}</strong><span>   <span class="remove-btn btn btn-secondary">-</span>  <span class="add-to-cart-btn btn btn-secondary">+</span></div>`
+        populate(element, drinksfoodContainer);
     })
 
 }).then(() => {
     document.querySelectorAll('.add-to-cart-btn').forEach(item => {
         item.addEventListener('click', (e) => {
             var parentElement = e.target.parentElement.parentElement.parentElement;
-            addToCart(parentElement.dataset.name, parentElement.dataset.price);
+            cartHandler(parentElement.dataset.name);
         })
     });
     document.querySelectorAll('.remove-btn').forEach(item => {
         item.addEventListener('click', (e) => {
             var parentElement = e.target.parentElement.parentElement.parentElement;
-            decreaseQuantity(parentElement.dataset.name);
+            cartHandler(parentElement.dataset.name, "remove");
         })
     });
 })
@@ -148,49 +136,31 @@ document.getElementById('totalBtn').addEventListener('click', () => {
 })
 
 /**
- * adds an item to the cart
+ * unhides the clicked product in cart
  */
-function addToCart(name, price) {
-    if (alreadyExists(name) == name) {
-        increaseQuantity(name);
-    } else {
-        var cart = document.getElementById('cart');
-        cart.innerHTML += `<div class="cart-item mt-2 mb-4">
-    <span class="cart-name">${name}</span>    
-    <input class="cart-quantity" placeholder="0" type="number" min="0" max="5" readOnly>
-    <span class="cart-price">${price} /pc</span>
-    </div>`
-    }
-}
-/**
- * increases cart item quantity by 1
- */
-function increaseQuantity(name) {
-    var cartitems = document.getElementsByClassName('cart-item')
-    for (var i = 0; i < cartitems.length; i++) {
-        var cartName = cartitems[i].getElementsByClassName('cart-name')[0].innerText;;
+function cartHandler(name, action = "add") {
+    var cartItems = document.getElementsByClassName('cart-item')
+    for (var i = 0; i < cartItems.length; i++) {
+        var cartName = cartItems[i].getElementsByClassName('cart-name')[0].innerText;
         if (cartName == name) {
-            if (cartitems[i].getElementsByClassName('cart-quantity')[0].value < 5) {
-                cartitems[i].getElementsByClassName('cart-quantity')[0].value++;
+            cartItems[i].classList.remove('d-none')
+                //increases cart item quantity by 1
+            if (action == "add") {
+                if (cartItems[i].getElementsByClassName('cart-quantity')[0].value < 5) {
+                    cartItems[i].getElementsByClassName('cart-quantity')[0].value++;
+                }
+            } else {
+                if (cartItems[i].getElementsByClassName('cart-quantity')[0].value > 1) {
+                    cartItems[i].getElementsByClassName('cart-quantity')[0].value--
+                } else {
+                    cartItems[i].getElementsByClassName('cart-quantity')[0].value = 0;
+                    cartItems[i].classList.add('d-none')
+                }
             }
         }
 
     }
-}
-/**
- * decreases cart item quantity by 1
- */
-function decreaseQuantity(name) {
-    var cartitems = document.getElementsByClassName('cart-item')
-    for (var i = 0; i < cartitems.length; i++) {
-        var cartName = cartitems[i].getElementsByClassName('cart-name')[0].innerText;
-        if (cartName == name) {
-            if (cartitems[i].getElementsByClassName('cart-quantity')[0].value > 0) {
-                cartitems[i].getElementsByClassName('cart-quantity')[0].value--
-            }
-        }
 
-    }
 }
 
 /**
@@ -209,24 +179,11 @@ function updatePrice() {
     document.getElementsByClassName('cart-total')[0].innerHTML = `Total: € ${total} `;
 }
 
-/**
- * checks to see if an item is already present in cart, if present, it will return the item
- */
-function alreadyExists(name) {
-    var cartItems = document.getElementsByClassName('cart-item')
-    for (var i = 0; i < cartItems.length; i++) {
-        var cartName = cartItems[i].getElementsByClassName('cart-name')[0].innerText;
-        if (cartName == name) {
-            return cartName;
-        }
-    }
 
-}
 document.getElementById('checkout-btn').addEventListener('click', () => {
     var billItems = checkout();
 
 })
-
 
 
 /**
